f99fefdd6860429a5bbba318bc66ffecea09d2db
ajayn710 <ajaynlumia710@gmail.com>
2016-10-14 10:33:53 -0700

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,9 @@
+<h1 style="color:#000000"><b><center>Contact Details</center></b></h1>
+<div align="center">
+<p><b>Address :</b> Army Institute of Technology, Dighi
+<p><b>Phone :</b> 020-456745<br>
+<b>Mobile :</b> +91 9015 501 897
+</p>
+<p><b>Email :</b> aitpune@gmail.com</p>
+</p>
+</div>
